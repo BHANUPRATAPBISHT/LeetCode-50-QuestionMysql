14aa9a35f01d45afa361f752e5b7bf62d09fbf23
BHANUPRATAPBISHT <168932555+BHANUPRATAPBISHT@users.noreply.github.com>
2026-01-17 02:25:36 +0530

    Time: 148 ms (5.01%) | Memory: 48.1 MB (11.33%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -3,6 +3,7 @@ class Solution {
 
         for(int i=0;i<nums.length-1;i++)
         {
+            boolean swap=false;
             for(int j=0;j<nums.length-1-i;j++)
             {
                 if(nums[j]==0 && nums[j+1]!=0)
@@ -10,7 +11,12 @@ class Solution {
                     int temp=nums[j];
                     nums[j]=nums[j+1];
                     nums[j+1]=temp;
+                    swap=true;
                 }
+            }
+            if(swap==false)
+            {
+                break;
             }
         }
         
