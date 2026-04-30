f45ef2425179e3457096c73ad00e3b3560ba417e
BHANUPRATAPBISHT <168932555+BHANUPRATAPBISHT@users.noreply.github.com>
2026-04-30 13:07:06 +0530

    Time: 1 ms (99.96%) | Memory: 42.6 MB (63.71%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -1,16 +1,21 @@
 class Solution {
     public int reverse(int x) {
-        int res = 0;
-        boolean isNegative = x < 0;
-        String strX = String.valueOf(Math.abs(x));
-        StringBuilder sb = new StringBuilder(strX).reverse();
+        int num = Math.abs(x);  // Original number ka absolute value nikala
         
-        try {
-            res = Integer.parseInt(sb.toString());
-        } catch (NumberFormatException e) {
-            return 0;
+        int rev = 0;  // Reversed number
+        
+        while (num != 0) {
+            int ld = num % 10;  // Last digit nikala
+            
+            // Overflow check
+            if (rev > (Integer.MAX_VALUE - ld) / 10) {
+                return 0;  // Agar overflow hua, toh 0 return kardo
+            }
+            
+            rev = rev * 10 + ld;  // Reverse mein digit ko add kiya
+            num = num / 10;  // Last digit hata diya, next iteration ke liye
         }
         
-        return isNegative ? -res : res;       
+        return (x < 0) ? (-rev) : rev;  // Original number ke sign ke hisaab se result diya
     }
 }
